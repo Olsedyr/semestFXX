0bdf50e4a21cd19c8b646033ad0c79bf0a8b652f
louip22 <louip22@student.sdu.dk>
2022-11-30 09:43:37 +0100

    Help Button + Displaybox
    Help command fully functional in all scenes, and display box only visible when activated and will disappear again when help button is pressed second time.

--- src/main/java/com/example/semestfxx/GameText.java
+++ src/main/java/com/example/semestfxx/GameText.java
@@ -5,6 +5,8 @@ import javafx.scene.text.Text;
 public class GameText {
 
     public static String textHelp() {
-        return "Help is here!";
+        return "Du er inde i en verden hvor du skal træffe de rigtige valg for klimaet. Du starter med 0 point, og dit mål er at få så mange point som muligt, ud fra dine klimabevidste handlinger. " +
+                "Du kan få mere at vide om de enkelte objekter ved at højreklikke på dem om bruge dem/ samle ting op ved at venstreklikke. Du kan bruge døre og pile til at navigere mellem rummene. " +
+                "Prøv at undersøge dine omgivelser og se om du kan gøre en forskel!";
     }
 }
